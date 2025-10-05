5c4050e5f32cd7f5fd2f2c2c2d5eaf390af611e4
Riadul Islam <riadul551@gmail.com>
2025-10-05 10:36:04 +0600

    change some design of pricing card

--- src/components/pricingOptions/PricingCard.jsx
+++ src/components/pricingOptions/PricingCard.jsx
@@ -5,18 +5,18 @@ const PricingCard = ({ card }) => {
   const { membership, duration, price, description, facilities } = card;
   //   console.log(card);
   return (
-    <div className="border rounded-2xl py-2 px-4 ">
+    <div className="flex flex-col rounded-2xl py-2 px-4 shadow-2xl border-gray-900">
       <h1 className="text-xl font-bold mb-3">{membership}</h1>
       <h4>{duration}</h4>
       <button className="btn btn-primary w-full">${price} / Month</button>
-      <p className="my-3 text-sm text-justify px-3">{description}</p>
+      <p className="flex-1 my-3 text-sm text-justify px-3">{description}</p>
       <h3 className="font-bold mt-3">Facilities : </h3>
-      <div>
+      <div className="flex-1">
         {facilities.map((facility) => (
           <Facility facility={facility}></Facility>
         ))}
       </div>
-      <div className="mt-3 text-end">
+      <div className="mt-6 text-end">
         <button className="btn">Purches</button>
       </div>
     </div>
--- src/components/pricingOptions/Pricingoptions.jsx
+++ src/components/pricingOptions/Pricingoptions.jsx
@@ -6,7 +6,7 @@ const Pricingoptions = ({ pricingOption }) => {
   const pricingData = use(pricingOption);
   console.log(pricingData);
   return (
-    <div>
+    <div className="mb-14">
       <h1 className="text-center font-bold mt-9 mb-4 text-2xl">Gym Plan</h1>
       <div className=" grid grid-cols-1 md:grid-cols-3 gap-8 px-3 max-w-[1200px] mx-auto">
         {pricingData.map((card) => (
